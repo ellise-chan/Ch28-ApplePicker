1120bceb3510236ea4582f035d3504df89813aab
ellise-chan <49345342+ellise-chan@users.noreply.github.com>
2019-09-27 02:39:26 -0400

    Created HighScore Script and Update Scripts

--- Assets/ApplePicker.cs
+++ Assets/ApplePicker.cs
@@ -43,11 +43,11 @@ public class ApplePicker : MonoBehaviour
         //Remove the Basket from the list and destroy the GameObject
         basketList.RemoveAt(basketIndex);
         Destroy(tBasketGO);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
         
+        //If there are no Baskets left, restart the game
+        if(basketList.Count == 0)
+        {
+            SceneManager.LoadScene("_Scene_0");
+        }
     }
 }
